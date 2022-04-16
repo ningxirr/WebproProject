df9322c6ce05cb459db08497cd41190f956c089c
ningxirr <pojanut.ara@student.mahidol.edu>
2022-04-16 17:03:30 +0700

    changed to mongo atlas

--- restAPI/server.js
+++ restAPI/server.js
@@ -10,7 +10,7 @@ Account = require('./api/models/loginModel')
 bodyParser = require('body-parser')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/ContactList', function(error){
+mongoose.connect('mongodb+srv://Pojanut:1234@cluster0.cwpvt.mongodb.net/ContactList?retryWrites=true&w=majority', function(error){
     if(error) throw error
     console.log('Successfully connected');
 })
@@ -27,5 +27,10 @@ var routeslogin = require('./api/routes/loginRoutes')
 routes(app)
 routeslogin(app)
 
+let port = process.env.PORT;
+if(port==null || port==""){
+    port = 5000;
+}
+
 app.listen(port)
-console.log('User List API started on : '+ port)
+console.log('Contact List API started on : '+ port)
